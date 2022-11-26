38ba71adc1754ce2c46ab512e30cf232fe408d5a
NadezhdaKuzmich <speranza.kuzmich@gmail.com>
2022-11-26 01:53:34 +0100

    add homework

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let name = prompt('What is your name?');
+alert(`Hello, ${name}! How are you?`);
+
+/*
+another way:
+
+let name = prompt('What is your name?');
+let message = `Hello, ${name}! How are you?`;
+
+alert(message);
+*/
